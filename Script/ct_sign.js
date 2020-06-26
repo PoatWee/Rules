dda1252b8b8336567b279a945856a1e6d5166af9
PoatWee <aw0120@126.com>
2020-06-26 23:21:47 +0800

    Create ct_sign.js

---
+++ Script/ct_sign.js
@@ -0,0 +1,211 @@
+/*
+    本作品用于QuantumultX和Surge之间js执行方法的转换
+    您只需书写其中任一软件的js,然后在您的js最【前面】追加上此段js即可
+    无需担心影响执行问题,具体原理是将QX和Surge的方法转换为互相可调用的方法
+    尚未测试是否支持import的方式进行使用,因此暂未export
+    如有问题或您有更好的改进方案,请前往 https://github.com/sazs34/TaskConfig/issues 提交内容,或直接进行pull request
+*/
+// #region 固定头部
+let isQuantumultX = $task != undefined; //判断当前运行环境是否是qx
+let isSurge = $httpClient != undefined; //判断当前运行环境是否是surge
+// http请求
+var $task = isQuantumultX ? $task : {};
+var $httpClient = isSurge ? $httpClient : {};
+// cookie读写
+var $prefs = isQuantumultX ? $prefs : {};
+var $persistentStore = isSurge ? $persistentStore : {};
+// 消息通知
+var $notify = isQuantumultX ? $notify : {};
+var $notification = isSurge ? $notification : {};
+// #endregion 固定头部
+
+// #region 网络请求专用转换
+if (isQuantumultX) {
+    var errorInfo = {
+        error: ''
+    };
+    $httpClient = {
+        get: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        },
+        post: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            url.method = 'POST';
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        }
+    }
+}
+if (isSurge) {
+    $task = {
+        fetch: url => {
+            //为了兼容qx中fetch的写法,所以永不reject
+            return new Promise((resolve, reject) => {
+                if (url.method == 'POST') {
+                    $httpClient.post(url, (error, response, data) => {
+                        if (response) {
+                            response.body = data;
+                            resolve(response, {
+                                error: error
+                            });
+                        } else {
+                            resolve(null, {
+                                error: error
+                            })
+                        }
+                    })
+                } else {
+                    $httpClient.get(url, (error, response, data) => {
+                        if (response) {
+                            response.body = data;
+                            resolve(response, {
+                                error: error
+                            });
+                        } else {
+                            resolve(null, {
+                                error: error
+                            })
+                        }
+                    })
+                }
+            })
+
+        }
+    }
+}
+// #endregion 网络请求专用转换
+
+// #region cookie操作
+if (isQuantumultX) {
+    $persistentStore = {
+        read: key => {
+            return $prefs.valueForKey(key);
+        },
+        write: (val, key) => {
+            return $prefs.setValueForKey(val, key);
+        }
+    }
+}
+if (isSurge) {
+    $prefs = {
+        valueForKey: key => {
+            return $persistentStore.read(key);
+        },
+        setValueForKey: (val, key) => {
+            return $persistentStore.write(val, key);
+        }
+    }
+}
+// #endregion
+
+// #region 消息通知
+if (isQuantumultX) {
+    $notification = {
+        post: (title, subTitle, detail) => {
+            $notify(title, subTitle, detail);
+        }
+    }
+}
+if (isSurge) {
+    $notify = function (title, subTitle, detail) {
+        $notification.post(title, subTitle, detail);
+    }
+}
+// #endregion
+
+const cookieName = '携程小程序签到'
+const bodyKey = 'chen_body_xc'
+const headerKey = 'chen_header_xc'
+const chen = init()
+let bodyVal = chen.getdata(bodyKey)
+let headerVal = chen.getdata(headerKey)
+sign()
+function sign() {
+    let url = {url: 'https://m.ctrip.com/restapi/soa2/16575/signin',headers: JSON.parse(headerVal),body: JSON.parse(bodyVal)}
+    chen.post(url, (error, response, data) => {
+        chen.log(`${cookieName}, data: ${data}`)
+        const result = JSON.parse(data)
+        let subTitle = ``
+        let detail = ``
+        if (result.errcode == 0) {
+          subTitle = `签到结果: 成功`
+          detail = `说明: ${result.errmsg}`
+        } else if (result.errcode == 3) {
+          subTitle = `签到结果: 成功 (重复签到)`
+          detail = `说明: ${result.errmsg}`
+        } else {
+          subTitle = `签到结果: 失败`
+          detail = `说明: ${result.errmsg}`
+        }
+        chen.msg(cookieName, subTitle, detail)
+        chen.done()
+      })
+    }
+function init() {
+    isSurge = () => {
+      return undefined === this.$httpClient ? false : true
+    }
+    isQuanX = () => {
+      return undefined === this.$task ? false : true
+    }
+    getdata = (key) => {
+      if (isSurge()) return $persistentStore.read(key)
+      if (isQuanX()) return $prefs.valueForKey(key)
+    }
+    setdata = (key, val) => {
+      if (isSurge()) return $persistentStore.write(key, val)
+      if (isQuanX()) return $prefs.setValueForKey(key, val)
+    }
+    msg = (title, subtitle, body) => {
+      if (isSurge()) $notification.post(title, subtitle, body)
+      if (isQuanX()) $notify(title, subtitle, body)
+    }
+    log = (message) => console.log(message)
+    get = (url, cb) => {
+      if (isSurge()) {
+        $httpClient.get(url, cb)
+      }
+      if (isQuanX()) {
+        url.method = 'GET'
+        $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+      }
+    }
+    post = (url, cb) => {
+      if (isSurge()) {
+        $httpClient.post(url, cb)
+      }
+      if (isQuanX()) {
+        url.method = 'POST'
+        $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+      }
+    }
+    done = (value = {}) => {
+      $done(value)
+    }
+    return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+  }
+  chen.done()
